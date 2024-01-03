d8620e2fac9136bad7d092bae6221fdc8527b04a
ernstmv <ernestoroque777@gmail.com>
2024-01-03 16:40:23 -0600

    functional live-stream

--- main.py
+++ main.py
@@ -2,41 +2,34 @@ import sys
 import cv2
 import mediapipe as mp
 
-def accesing_cam():
-    s=0
-    if len(sys.argv) > 1: s = sys.argv[1]
-    return cv2.VideoCapture(s)
+WIN_NAME = "Camera"
+
+
+mp_drw =  mp.solutions.drawing_utils
+mp_drw_stl = mp.solutions.drawing_styles
+mp_hnds = mp.solutions.hands
+hands=mp_hnds.Hands()
 
 def main():
-    win_name = "Camera"
-    cv2.namedWindow(win_name, cv2.WINDOW_NORMAL)
-    source=accesing_cam()
-    mp_drw =  mp.solutions.drawing_utils
-    mp_drw_stl = mp.solutions.drawing_styles
-    mp_hnds = mp.solutions.hands
+    cv2.namedWindow(WIN_NAME, cv2.WINDOW_NORMAL)
 
-    while cv2.waitKey(1) != 27:
+    s=0
+    if len(sys.argv) > 1: s = sys.argv[1]
+    source=cv2.VideoCapture(s)
+
+    while source.isOpened():
         has_frame, frame = source.read()
         if not has_frame: break
-        frame = cv2.flip(frame, 1)
+        frame=cv2.flip(frame, 1)
+        results = hands.process(frame)
+        try:
+            mp_drw.draw_landmarks(frame, results.multi_hand_landmarks[-1], mp_hnds.HAND_CONNECTIONS)
+        except Exception: pass
+        cv2.imshow(WIN_NAME, frame)
 
-        with mp_hnds.Hands(
-                static_image_mode = True,
-                max_num_hands = 2,
-                min_tracking_confidence = 0.5,
-                min_detection_confidence = 0.6) as hands:
-            results=hands.process(frame)
-            try:
-                mp_drw.draw_landmarks(
-                        frame, 
-                        results.multi_hand_landmarks[-1],
-                        mp_hnds.HAND_CONNECTIONS, 
-                        mp_drw_stl.DrawingSpec(color = (255,0, 255), thickness = 4))
-            except Exception: pass
-        cv2.imshow(win_name, frame)
-
+        if cv2.waitKey(1) & 0xFF == ord('q'): break
     source.release()
-    cv2.destroyWindow(win_name)
+    cv2.destroyWindow(WIN_NAME)
 
 if __name__ == "__main__":
     main()
